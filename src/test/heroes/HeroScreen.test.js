e51d817086e7b7fae146b7bb7ef3dea923968f46
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-08-17 22:35:15 -0300

    test screen

--- src/test/heroes/HeroScreen.test.js
+++ src/test/heroes/HeroScreen.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {mount,shallow} from 'enzyme';
 import '@testing-library/jest-dom';
 import HeroScreen from '../../components/heroes/HeroScreen';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
 
     describe('Pruebas en HeroScree/>', () => {
 
@@ -24,12 +24,58 @@ import { MemoryRouter } from 'react-router-dom';
         });
 
         test('muestra el hero ssi el param exist', () => {
+
             const wrapper=mount(
-                <MemoryRouter initialEntries={['/hero/dc-green']}>
-                    <HeroScreen history={historyMock}/>
+                <MemoryRouter initialEntries={['/hero/marvel-spider']}>
+                    <Route path="/hero/:heroeId" component={HeroScreen}/>
                 </MemoryRouter>
             );
-        })
+            expect(wrapper.find('.row').exists()).toBe(true);
+        });
+
+        test('regresa a la pantalla anterior con history.PUSH', () => {
+            const historyMock={
+                length:1,
+                push:jest.fn(),
+                goBack:jest.fn()
+            }
+            const wrapper=mount(
+                <MemoryRouter initialEntries={['/hero/marvel-spider']}>
+                    <Route 
+                        path="/hero/:heroeId" 
+                        component={(props)=><HeroScreen history={historyMock}/>}/>
+                </MemoryRouter>
+            );
+            wrapper.find('button').prop('onClick')();
+            expect(historyMock.push).toHaveBeenCalledWith('/');
+            expect(historyMock.goBack).not.toHaveBeenCalled();
+        });
+        test('regresa a la pantalla anterior GoBack', () => {
+
+            const wrapper=mount(
+                <MemoryRouter initialEntries={['/hero/marvel-spider']}>
+                    <Route 
+                        path="/hero/:heroeId" 
+                        component={(props)=><HeroScreen history={historyMock}/>}/>
+                </MemoryRouter>
+            );
+            wrapper.find('button').prop('onClick')();
+            expect(historyMock.push).toHaveBeenCalledTimes(0);
+            expect(historyMock.goBack).toHaveBeenCalled();
+        });
+        test('param mal REDICERT', () => {
+            
+
+            const wrapper=mount(
+                <MemoryRouter initialEntries={['/hero/marvel-spider123123']}>
+                    <Route 
+                        path="/hero/:heroeId" 
+                        component={(props)=><HeroScreen history={historyMock}/>}/>
+                </MemoryRouter>
+            );
+                expect(wrapper.text()).toBe('');
+        });
+        
         
 
         
